78fc963d3c3d04abd0dd19487558b88e43ccfbcc
Jamaludin Fikri <jamaludinfikrii@gmail.com>
2020-10-05 11:55:28 +0700

    socket event and emit

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const httpApp = http.createServer(app)
 const PORT = 5000
 const io = socket(httpApp)
 
+let userConnected = []
+io.on('connection' , (socket) => {
+    userConnected.push(socket.id)
+    console.log('user connected with id ' + socket.id)
+
+    socket.on('test', (data) => {
+        console.log(data)
+        socket.emit('test' , data + " susilo")
+    })
+
+    socket.on('disconnect' , () => {
+        let index = userConnected.indexOf(socket.id)
+        console.log('user disconnected with id = ' + userConnected[index])
+    })
+})
+
 // routes
 app.get('/' , (req,res) => {
     res.send("Welocome to API Chat")
---
+++ notes.js
@@ -0,0 +1,7 @@
+// socket io buat realtime 
+// ada dua hal yang perlu dipahami
+    // event (route name) 
+    // emit (response)
+
+    // on itu buat nerima data / listen event
+    // emit itu buat kirim data / send request
